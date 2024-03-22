1e037e899142eea4b8de117a3ef113a0adc0fd27
cyber <aziz.sabyrbaev@bk.ru>
2024-03-22 23:28:01 +0300

    mocking our db store and adding unit test for our service

---
+++ internal/rocket/rocket.go
@@ -0,0 +1,61 @@
+//go:generate mockgen -destination=rocket_mocks_test.go -package=rocket github.com/instinctG/simple-grpc-service/internal/rocket Store
+
+package rocket
+
+import "context"
+
+// Rocket - should contain things like the ID for the rocket,
+// the name for the rocket and the type of rocket.
+type Rocket struct {
+	ID   string
+	Name string
+	Type string
+}
+
+// Store - defines the interface we need to satisfy for our
+// service to work correctly
+type Store interface {
+	GetRocketByID(ctx context.Context, ID string) (Rocket, error)
+	InsertRocket(ctx context.Context, rocket Rocket) (Rocket, error)
+	DeleteRocket(ID string) error
+}
+
+// Service - our rocket service, used for updating our
+// rocket inventory
+type Service struct {
+	Store Store
+}
+
+// New - returns a new rocket service
+func New(store Store) Service {
+	return Service{Store: store}
+}
+
+// GetRocketByID - retrieves a rocket from the store by ID
+func (s Service) GetRocketByID(ctx context.Context, id string) (Rocket, error) {
+	rkt, err := s.Store.GetRocketByID(ctx, id)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+}
+
+// InsertRocket - Adds a rocket to our store
+func (s Service) InsertRocket(ctx context.Context, rocket Rocket) (Rocket, error) {
+	rkt, err := s.Store.InsertRocket(ctx, rocket)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+}
+
+// DeleteRocket - deletes a rocket - most likely rapid
+// unscheduled disassembly
+func (s Service) DeleteRocket(id string) error {
+	err := s.Store.DeleteRocket(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/rocket/rocket_test.go
@@ -0,0 +1,56 @@
+package rocket
+
+import (
+	"context"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRocketService(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	t.Run("tests get rocket by id", func(t *testing.T) {
+		rocketStoreMock := NewMockStore(mockCtrl)
+		id := "UUID-1"
+		rocketStoreMock.
+			EXPECT().
+			GetRocketByID(context.Background(), id).
+			Return(Rocket{
+				ID: id,
+			}, nil)
+
+		rocketService := New(rocketStoreMock)
+		rkt, err := rocketService.GetRocketByID(context.Background(), id)
+		assert.NoError(t, err)
+		assert.Equal(t, "UUID-1", rkt.ID)
+	})
+
+	t.Run("tests insert rocket", func(t *testing.T) {
+		rocketStoreMock := NewMockStore(mockCtrl)
+		id := "UUID-1"
+		rocketStoreMock.
+			EXPECT().
+			InsertRocket(context.Background(), Rocket{ID: id}).
+			Return(Rocket{ID: id}, nil)
+
+		rocketService := New(rocketStoreMock)
+		rkt, err := rocketService.InsertRocket(context.Background(), Rocket{ID: id})
+		assert.NoError(t, err)
+		assert.Equal(t, "UUID-1", rkt.ID)
+	})
+
+	t.Run("tests delete rocket", func(t *testing.T) {
+		rocketStoreMock := NewMockStore(mockCtrl)
+		id := "UUID-1"
+		rocketStoreMock.
+			EXPECT().
+			DeleteRocket(id).
+			Return(nil)
+
+		rocketService := New(rocketStoreMock)
+		err := rocketService.DeleteRocket(id)
+		assert.NoError(t, err)
+	})
+}
